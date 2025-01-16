cdeaaf2cdfbdb712fa0841adff425677ad74b579
LucasDaenenPXL <daenenlucas@gmail.com>
2025-01-16 12:29:34 +0100

    alles

---
+++ g-link/src/main/java/Beesten/g_link/GLinkApplication.java
@@ -0,0 +1,13 @@
+package Beesten.g_link;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GLinkApplsication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GLinkApplication.class, args);
+	}
+
+}
---
+++ g-link/src/test/java/Beesten/g_link/GLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package Beesten.g_link;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
